891446dcc5450a61f62ca8e62afa8a497b68f3b6
Oleksandr Kolodkin <alexandr.kolodkin@gmail.com>
2021-03-27 08:07:04 +0200

    Update translation

--- recon_ru.ts
+++ recon_ru.ts
@@ -4,27 +4,27 @@
 <context>
     <name>FileDialog</name>
     <message>
-        <location filename="recon.py" line="362"/>
+        <location filename="recon.py" line="604"/>
         <source>Recon data files(*.txt)</source>
         <translation>Файл данных рекона(*.txt)</translation>
     </message>
     <message>
-        <location filename="recon.py" line="345"/>
+        <location filename="recon.py" line="587"/>
         <source>Open recon data file</source>
         <translation>Открыть файл данных рекона</translation>
     </message>
     <message>
-        <location filename="recon.py" line="379"/>
+        <location filename="recon.py" line="621"/>
         <source>PNG Image(*.png);;SVG Image(*.svg);;PDF Document(*.pdf)</source>
         <translation>Изображение PNG(*.png);;Изображение SVG(*.svg);;Документ PDF(*.pdf)</translation>
     </message>
     <message>
-        <location filename="recon.py" line="380"/>
+        <location filename="recon.py" line="622"/>
         <source>Save plot</source>
         <translation>Сохранить график</translation>
     </message>
     <message>
-        <location filename="recon.py" line="363"/>
+        <location filename="recon.py" line="605"/>
         <source>Save recon data file</source>
         <translation>Сохранить данные рекона в файл</translation>
     </message>
@@ -32,7 +32,7 @@
 <context>
     <name>Main</name>
     <message>
-        <location filename="recon.py" line="501"/>
+        <location filename="recon.py" line="744"/>
         <source>Recon plotter - {0}</source>
         <translation></translation>
     </message>
@@ -40,185 +40,248 @@
 <context>
     <name>Menu</name>
     <message>
-        <location filename="recon.py" line="176"/>
+        <location filename="recon.py" line="411"/>
         <source>&amp;Exit</source>
         <translation>&amp;Выход</translation>
     </message>
     <message>
-        <location filename="recon.py" line="177"/>
+        <location filename="recon.py" line="412"/>
         <source>Ctrl+Q</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="recon.py" line="178"/>
+        <location filename="recon.py" line="413"/>
         <source>Exit application</source>
         <translation>Выйти из приложения</translation>
     </message>
     <message>
-        <location filename="recon.py" line="159"/>
+        <location filename="recon.py" line="394"/>
         <source>&amp;Open...</source>
         <translation>&amp;Открыть...</translation>
     </message>
     <message>
-        <location filename="recon.py" line="160"/>
+        <location filename="recon.py" line="395"/>
         <source>Ctrl+O</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="recon.py" line="161"/>
+        <location filename="recon.py" line="396"/>
         <source>Open the recon data in the text format</source>
         <translation>Открыть данные рекона в текстовом формате</translation>
     </message>
     <message>
-        <location filename="recon.py" line="164"/>
+        <location filename="recon.py" line="399"/>
         <source>&amp;Save</source>
         <translation>&amp;Сохранить</translation>
     </message>
     <message>
-        <location filename="recon.py" line="165"/>
+        <location filename="recon.py" line="400"/>
         <source>Ctrl+S</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="recon.py" line="190"/>
+        <location filename="recon.py" line="425"/>
         <source>Save plot</source>
         <translation>Сохранить график</translation>
     </message>
     <message>
-        <location filename="recon.py" line="170"/>
+        <location filename="recon.py" line="405"/>
         <source>Save &amp;as...</source>
         <translation>Сохранить &amp;как...</translation>
     </message>
     <message>
-        <location filename="recon.py" line="171"/>
+        <location filename="recon.py" line="406"/>
         <source>Ctrl+Shift+S</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="recon.py" line="196"/>
+        <location filename="recon.py" line="431"/>
         <source>Save plot as...</source>
         <translation>Сохранить график как...</translation>
     </message>
     <message>
-        <location filename="recon.py" line="202"/>
+        <location filename="recon.py" line="437"/>
         <source>Show/hide signals window</source>
         <translation>Показать/скрыть окно сигналов</translation>
     </message>
     <message>
-        <location filename="recon.py" line="205"/>
+        <location filename="recon.py" line="440"/>
         <source>About Qt...</source>
         <translation>О Qt...</translation>
     </message>
     <message>
-        <location filename="recon.py" line="210"/>
+        <location filename="recon.py" line="445"/>
         <source>&amp;File</source>
         <translation>&amp;Файл</translation>
     </message>
     <message>
-        <location filename="recon.py" line="220"/>
+        <location filename="recon.py" line="455"/>
         <source>&amp;View</source>
         <translation>&amp;Вид</translation>
     </message>
     <message>
-        <location filename="recon.py" line="223"/>
+        <location filename="recon.py" line="458"/>
         <source>&amp;Help</source>
         <translation>&amp;Помощь</translation>
     </message>
     <message>
-        <location filename="recon.py" line="166"/>
+        <location filename="recon.py" line="401"/>
         <source>Save the recon data in the text format</source>
         <translation>Сохранить данные рекона в текстовом формате</translation>
     </message>
     <message>
-        <location filename="recon.py" line="173"/>
+        <location filename="recon.py" line="408"/>
         <source>Save the recon data in the text format as...</source>
         <translation>Сохранить данные рекона в текстовом формате как...</translation>
     </message>
     <message>
-        <location filename="recon.py" line="188"/>
+        <location filename="recon.py" line="423"/>
         <source>&amp;Save plot</source>
         <translation>&amp;Сохранить график</translation>
     </message>
     <message>
-        <location filename="recon.py" line="189"/>
+        <location filename="recon.py" line="424"/>
         <source>Ctrl+Alt+S</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="recon.py" line="194"/>
+        <location filename="recon.py" line="429"/>
         <source>Save plot &amp;as...</source>
         <translation>Сохранить график &amp;как...</translation>
     </message>
     <message>
-        <location filename="recon.py" line="195"/>
+        <location filename="recon.py" line="430"/>
         <source>Ctrl+Alt+Shift+S</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="recon.py" line="215"/>
+        <location filename="recon.py" line="450"/>
         <source>&amp;Plot</source>
         <translation>&amp;График</translation>
     </message>
     <message>
-        <location filename="recon.py" line="182"/>
+        <location filename="recon.py" line="417"/>
         <source>&amp;Update</source>
         <translation>&amp;Обновить</translation>
     </message>
     <message>
-        <location filename="recon.py" line="183"/>
+        <location filename="recon.py" line="418"/>
         <source>F5</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="recon.py" line="184"/>
+        <location filename="recon.py" line="419"/>
         <source>Update plot</source>
         <translation>Обновить график</translation>
     </message>
 </context>
 <context>
+    <name>PlotSettingsDock</name>
+    <message>
+        <location filename="recon.py" line="276"/>
+        <source>Plot Settings</source>
+        <translation>Настройки графика</translation>
+    </message>
+    <message>
+        <location filename="recon.py" line="284"/>
+        <source>Title:</source>
+        <translation>Заголовок:</translation>
+    </message>
+    <message>
+        <location filename="recon.py" line="291"/>
+        <source>X axis label:</source>
+        <translation>Подпись оси x:</translation>
+    </message>
+    <message>
+        <location filename="recon.py" line="298"/>
+        <source>Y axis label:</source>
+        <translation>Подпись оси y:</translation>
+    </message>
+    <message>
+        <location filename="recon.py" line="305"/>
+        <source>X from:</source>
+        <translation>X от:</translation>
+    </message>
+    <message>
+        <location filename="recon.py" line="312"/>
+        <source>X to:</source>
+        <translation>X до:</translation>
+    </message>
+    <message>
+        <location filename="recon.py" line="319"/>
+        <source>Y from:</source>
+        <translation>Y от:</translation>
+    </message>
+    <message>
+        <location filename="recon.py" line="326"/>
+        <source>Y to:</source>
+        <translation>Y до:</translation>
+    </message>
+    <message>
+        <location filename="recon.py" line="333"/>
+        <source>DPI:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="recon.py" line="342"/>
+        <source>{0} landscape</source>
+        <translation>{0} альбомная ориентация</translation>
+    </message>
+    <message>
+        <location filename="recon.py" line="344"/>
+        <source>{0} portrate</source>
+        <translation>{0} портретная ориентация</translation>
+    </message>
+    <message>
+        <location filename="recon.py" line="344"/>
+        <source>Page size:</source>
+        <translation>Размер страницы:</translation>
+    </message>
+</context>
+<context>
     <name>SignalsDock</name>
     <message>
-        <location filename="recon.py" line="141"/>
+        <location filename="recon.py" line="376"/>
         <source>Selected</source>
         <translation>Выбран</translation>
     </message>
     <message>
-        <location filename="recon.py" line="142"/>
+        <location filename="recon.py" line="377"/>
         <source>Name</source>
         <translation>Имя</translation>
     </message>
     <message>
-        <location filename="recon.py" line="143"/>
+        <location filename="recon.py" line="378"/>
         <source>Unit</source>
         <translation>Единицы</translation>
     </message>
     <message>
-        <location filename="recon.py" line="144"/>
+        <location filename="recon.py" line="379"/>
         <source>Smooth</source>
         <translation>Сглаживание</translation>
     </message>
     <message>
-        <location filename="recon.py" line="145"/>
+        <location filename="recon.py" line="380"/>
         <source>Scale</source>
         <translation>Масштаб</translation>
     </message>
     <message>
-        <location filename="recon.py" line="146"/>
+        <location filename="recon.py" line="381"/>
         <source>Minimum</source>
         <translation>Минимум</translation>
     </message>
     <message>
-        <location filename="recon.py" line="147"/>
+        <location filename="recon.py" line="382"/>
         <source>Maximum</source>
         <translation>Максимум</translation>
     </message>
     <message>
-        <location filename="recon.py" line="130"/>
+        <location filename="recon.py" line="365"/>
         <source>Signals</source>
         <translation>Сигналы</translation>
     </message>
     <message>
-        <location filename="recon.py" line="148"/>
+        <location filename="recon.py" line="383"/>
         <source>Color</source>
         <translation>Цвет</translation>
     </message>
--- recon_uk.ts
+++ recon_uk.ts
@@ -4,27 +4,27 @@
 <context>
     <name>FileDialog</name>
     <message>
-        <location filename="recon.py" line="362"/>
+        <location filename="recon.py" line="604"/>
         <source>Recon data files(*.txt)</source>
         <translation>Файл даних рекону(*.txt)</translation>
     </message>
     <message>
-        <location filename="recon.py" line="345"/>
+        <location filename="recon.py" line="587"/>
         <source>Open recon data file</source>
         <translation>Відкрити файл даних рекону</translation>
     </message>
     <message>
-        <location filename="recon.py" line="379"/>
+        <location filename="recon.py" line="621"/>
         <source>PNG Image(*.png);;SVG Image(*.svg);;PDF Document(*.pdf)</source>
         <translation>Зображення PNG(*.png);;Зображення SVG(*.svg);;Документ PDF(*.pdf)</translation>
     </message>
     <message>
-        <location filename="recon.py" line="380"/>
+        <location filename="recon.py" line="622"/>
         <source>Save plot</source>
         <translation>Зберегти графік</translation>
     </message>
     <message>
-        <location filename="recon.py" line="363"/>
+        <location filename="recon.py" line="605"/>
         <source>Save recon data file</source>
         <translation>Зберегти дані реконц у текстовому форматі</translation>
     </message>
@@ -32,7 +32,7 @@
 <context>
     <name>Main</name>
     <message>
-        <location filename="recon.py" line="501"/>
+        <location filename="recon.py" line="744"/>
         <source>Recon plotter - {0}</source>
         <translation></translation>
     </message>
@@ -40,185 +40,248 @@
 <context>
     <name>Menu</name>
     <message>
-        <location filename="recon.py" line="176"/>
+        <location filename="recon.py" line="411"/>
         <source>&amp;Exit</source>
         <translation>&amp;Вихід</translation>
     </message>
     <message>
-        <location filename="recon.py" line="177"/>
+        <location filename="recon.py" line="412"/>
         <source>Ctrl+Q</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="recon.py" line="178"/>
+        <location filename="recon.py" line="413"/>
         <source>Exit application</source>
         <translation>Вийти з додатку</translation>
     </message>
     <message>
-        <location filename="recon.py" line="159"/>
+        <location filename="recon.py" line="394"/>
         <source>&amp;Open...</source>
         <translation>&amp;Відкрити...</translation>
     </message>
     <message>
-        <location filename="recon.py" line="160"/>
+        <location filename="recon.py" line="395"/>
         <source>Ctrl+O</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="recon.py" line="161"/>
+        <location filename="recon.py" line="396"/>
         <source>Open the recon data in the text format</source>
         <translation>Відкрити файл даних рекону у текстовому форматі</translation>
     </message>
     <message>
-        <location filename="recon.py" line="164"/>
+        <location filename="recon.py" line="399"/>
         <source>&amp;Save</source>
         <translation>&amp;Зберегти</translation>
     </message>
     <message>
-        <location filename="recon.py" line="165"/>
+        <location filename="recon.py" line="400"/>
         <source>Ctrl+S</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="recon.py" line="190"/>
+        <location filename="recon.py" line="425"/>
         <source>Save plot</source>
         <translation>Зберегти графік</translation>
     </message>
     <message>
-        <location filename="recon.py" line="170"/>
+        <location filename="recon.py" line="405"/>
         <source>Save &amp;as...</source>
         <translation>Зберегти &amp;як...</translation>
     </message>
     <message>
-        <location filename="recon.py" line="171"/>
+        <location filename="recon.py" line="406"/>
         <source>Ctrl+Shift+S</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="recon.py" line="196"/>
+        <location filename="recon.py" line="431"/>
         <source>Save plot as...</source>
         <translation>Зберегти графік як...</translation>
     </message>
     <message>
-        <location filename="recon.py" line="202"/>
+        <location filename="recon.py" line="437"/>
         <source>Show/hide signals window</source>
         <translation>Показати/сховати вікно сигналів</translation>
     </message>
     <message>
-        <location filename="recon.py" line="205"/>
+        <location filename="recon.py" line="440"/>
         <source>About Qt...</source>
         <translation>О Qt...</translation>
     </message>
     <message>
-        <location filename="recon.py" line="210"/>
+        <location filename="recon.py" line="445"/>
         <source>&amp;File</source>
         <translation>&amp;Файл</translation>
     </message>
     <message>
-        <location filename="recon.py" line="220"/>
+        <location filename="recon.py" line="455"/>
         <source>&amp;View</source>
         <translation>&amp;Вид</translation>
     </message>
     <message>
-        <location filename="recon.py" line="223"/>
+        <location filename="recon.py" line="458"/>
         <source>&amp;Help</source>
         <translation>&amp;Допомога</translation>
     </message>
     <message>
-        <location filename="recon.py" line="166"/>
+        <location filename="recon.py" line="401"/>
         <source>Save the recon data in the text format</source>
         <translation>Зберегти дані рекону у текстовому форматі</translation>
     </message>
     <message>
-        <location filename="recon.py" line="173"/>
+        <location filename="recon.py" line="408"/>
         <source>Save the recon data in the text format as...</source>
         <translation>Зберегти дані рекону у текстовому форматі як...</translation>
     </message>
     <message>
-        <location filename="recon.py" line="188"/>
+        <location filename="recon.py" line="423"/>
         <source>&amp;Save plot</source>
         <translation>&amp;Зберегти графік</translation>
     </message>
     <message>
-        <location filename="recon.py" line="189"/>
+        <location filename="recon.py" line="424"/>
         <source>Ctrl+Alt+S</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="recon.py" line="194"/>
+        <location filename="recon.py" line="429"/>
         <source>Save plot &amp;as...</source>
         <translation>Зберегти графік &amp;як...</translation>
     </message>
     <message>
-        <location filename="recon.py" line="195"/>
+        <location filename="recon.py" line="430"/>
         <source>Ctrl+Alt+Shift+S</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="recon.py" line="215"/>
+        <location filename="recon.py" line="450"/>
         <source>&amp;Plot</source>
         <translation>&amp;Графік</translation>
     </message>
     <message>
-        <location filename="recon.py" line="182"/>
+        <location filename="recon.py" line="417"/>
         <source>&amp;Update</source>
         <translation>&amp;Оновити</translation>
     </message>
     <message>
-        <location filename="recon.py" line="183"/>
+        <location filename="recon.py" line="418"/>
         <source>F5</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="recon.py" line="184"/>
+        <location filename="recon.py" line="419"/>
         <source>Update plot</source>
         <translation>Оновити графік</translation>
     </message>
 </context>
 <context>
+    <name>PlotSettingsDock</name>
+    <message>
+        <location filename="recon.py" line="276"/>
+        <source>Plot Settings</source>
+        <translation>Налаштування графіку</translation>
+    </message>
+    <message>
+        <location filename="recon.py" line="284"/>
+        <source>Title:</source>
+        <translation>Заголовок:</translation>
+    </message>
+    <message>
+        <location filename="recon.py" line="291"/>
+        <source>X axis label:</source>
+        <translation>Мітка вісі х:</translation>
+    </message>
+    <message>
+        <location filename="recon.py" line="298"/>
+        <source>Y axis label:</source>
+        <translation>Мітка вісі y:</translation>
+    </message>
+    <message>
+        <location filename="recon.py" line="305"/>
+        <source>X from:</source>
+        <translation>X від:</translation>
+    </message>
+    <message>
+        <location filename="recon.py" line="312"/>
+        <source>X to:</source>
+        <translation>X до:</translation>
+    </message>
+    <message>
+        <location filename="recon.py" line="319"/>
+        <source>Y from:</source>
+        <translation>Y від:</translation>
+    </message>
+    <message>
+        <location filename="recon.py" line="326"/>
+        <source>Y to:</source>
+        <translation>Y до:</translation>
+    </message>
+    <message>
+        <location filename="recon.py" line="333"/>
+        <source>DPI:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="recon.py" line="342"/>
+        <source>{0} landscape</source>
+        <translation>{0} альбомна орієнтація</translation>
+    </message>
+    <message>
+        <location filename="recon.py" line="344"/>
+        <source>{0} portrate</source>
+        <translation>{0} портретна орієнтація</translation>
+    </message>
+    <message>
+        <location filename="recon.py" line="344"/>
+        <source>Page size:</source>
+        <translation>Розмір сторінки:</translation>
+    </message>
+</context>
+<context>
     <name>SignalsDock</name>
     <message>
-        <location filename="recon.py" line="141"/>
+        <location filename="recon.py" line="376"/>
         <source>Selected</source>
         <translation>Обрано</translation>
     </message>
     <message>
-        <location filename="recon.py" line="142"/>
+        <location filename="recon.py" line="377"/>
         <source>Name</source>
         <translation>Ім&apos;я</translation>
     </message>
     <message>
-        <location filename="recon.py" line="143"/>
+        <location filename="recon.py" line="378"/>
         <source>Unit</source>
         <translation>Одиниці</translation>
     </message>
     <message>
-        <location filename="recon.py" line="144"/>
+        <location filename="recon.py" line="379"/>
         <source>Smooth</source>
         <translation>Згладжування</translation>
     </message>
     <message>
-        <location filename="recon.py" line="145"/>
+        <location filename="recon.py" line="380"/>
         <source>Scale</source>
         <translation>Масштаб</translation>
     </message>
     <message>
-        <location filename="recon.py" line="146"/>
+        <location filename="recon.py" line="381"/>
         <source>Minimum</source>
         <translation>Мінімум</translation>
     </message>
     <message>
-        <location filename="recon.py" line="147"/>
+        <location filename="recon.py" line="382"/>
         <source>Maximum</source>
         <translation>Максімум</translation>
     </message>
     <message>
-        <location filename="recon.py" line="130"/>
+        <location filename="recon.py" line="365"/>
         <source>Signals</source>
         <translation>Сигнали</translation>
     </message>
     <message>
-        <location filename="recon.py" line="148"/>
+        <location filename="recon.py" line="383"/>
         <source>Color</source>
         <translation>Колір</translation>
     </message>
